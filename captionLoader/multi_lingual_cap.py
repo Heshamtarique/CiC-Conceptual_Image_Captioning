0ebe0d780ccee2612f32a1722887084fdd1e1399
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-04-22 23:15:17 +0530

    Update multi_lingual_cap.py

--- captionLoader/multi_lingual_cap.py
+++ captionLoader/multi_lingual_cap.py
@@ -1,1 +1,51 @@
+# importing relevant libraries
+
+import os
+import json
+# pip install opencv-python
+import cv2
+import numpy as np
+import pandas as pd
+
+
+
+# loading the Multi Lingual caption file...
+
+def load_jsonl_file1(file_path):
+    try:
+        df = pd.read_json(file_path, lines=True)
+        return df
+    except Exception as e:
+        print(f"Error loading JSONL file: {e}")
+        return None
+
+# Example usage:
+file_path = "/User/multi_lingual_captions.jsonl"
+df = load_jsonl_file1(file_path)
+# df = pd.DataFrame(data)
+# Check if DataFrame was successfully loaded
+if df is not None:
+    # Now you can use df for further data analysis
+    print("DataFrame loaded successfully!")
+    print(df.head())
+else:
+    print("Failed to load DataFrame.")
+
+
+# analysis 
+df.shape
+
+df.columns
+
+''' 
+Index(['image/key', 'image/locale', 'ar', 'bn', 'cs', 'da', 'de', 'el', 'en',
+       'es', 'fa', 'fi', 'fil', 'fr', 'hi', 'hr', 'hu', 'id', 'it', 'he', 'ja',
+       'ko', 'mi', 'nl', 'no', 'pl', 'pt', 'quz', 'ro', 'ru', 'sv', 'sw', 'te',
+       'th', 'tr', 'uk', 'vi', 'zh'],
+      dtype='object')
+'''
+df_en = df[['image/key','en']]
+df_en.tail(8)
+
+print(f"caption fora data in english:  {df_en['en'][3599]}")
 
